6705edab41b8f86702d26ef5276e3719c28ac226
JosueMart <josue.mart@live.com.mx>
2015-09-29 09:57:21 -0500

    Dibujo el ahorcado, sin tomar en cuenta las palabras

--- ahorcado.js
+++ ahorcado.js
@@ -38,6 +38,68 @@ Ahorcado.prototype.dibujar=function()
 		dibujo.lineWidth=5;
 		dibujo.stroke();
 		dibujo.closePath();
+		if(this.intentos > 1)
+		{
+		//intentos = 2 dibujo el torso
+		dibujo.beginPath();
+		dibujo.moveTo(150,180);
+		dibujo.lineTo(150,250);
+		dibujo.strokeStyle = "#F00"
+		dibujo.lineWidth=5;
+		dibujo.stroke();
+		dibujo.closePath();
+		if(this.intentos > 2)
+		{
+			//intentos = 3 dibujo los brazos
+			dibujo.beginPath();
+			dibujo.moveTo(120,220);
+			dibujo.lineTo(150,180);
+			dibujo.lineTo(180,220);
+
+			dibujo.strokeStyle = "#F00"
+			dibujo.lineWidth=5;
+			dibujo.stroke();
+			dibujo.closePath();
+
+		}
+		if(this.intentos > 3)
+		{
+			//intentos = 4 dibujo las piernas
+			dibujo.beginPath();
+			dibujo.moveTo(120,290);
+			dibujo.lineTo(150,250);
+			dibujo.lineTo(180,290);
+			dibujo.strokeStyle = "#F00"
+			dibujo.lineWidth=5;
+			dibujo.stroke();
+			dibujo.closePath();
+
+			if(this.intentos > 4)
+		{
+			//intentos = 5 dibujo ojos muertos
+			dibujo.beginPath();
+			//ojo izquierdo
+			dibujo.moveTo(125,120);
+			dibujo.lineTo(145,145);
+			dibujo.moveTo(145,120);
+			dibujo.lineTo(125,145);
+			//ojo derecho
+			dibujo.moveTo(155,120);
+			dibujo.lineTo(175,145);
+			dibujo.moveTo(175,120);
+			dibujo.lineTo(155,145);
+
+			dibujo.strokeStyle = "blue"
+			dibujo.lineWidth=5;
+			dibujo.stroke();
+			dibujo.closePath();
+
+
+
+		}
+
+		}
+		}
 
 	}
 
@@ -49,6 +111,7 @@ Ahorcado.prototype.trazar=function()
 	if(this.intentos >= this.maximo)
 	{
 		this.vivo=false;
+		alert("Estas muerto!!!");
 	}
 	this.dibujar();
 }
@@ -61,6 +124,14 @@ function iniciar()
 	var contexto = canvas.getContext("2d");
 	hombre=new Ahorcado(contexto);
 	hombre.trazar();
+	hombre.trazar();
+	hombre.trazar();
+	hombre.trazar();
+	hombre.trazar();
+
+
+
+
 
 
 }
